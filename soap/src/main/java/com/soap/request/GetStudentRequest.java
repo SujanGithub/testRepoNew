e1bbc53ddd77fd9048db081d4afdff24552e103e
Singh <703416496@genpact.com>
2025-03-20 10:46:06 +0530

    Commit

---
+++ soap/src/main/java/com/soap/request/GetStudentRequest.java
@@ -0,0 +1,17 @@
+ 
+package com.soap.request;
+ 
+import jakarta.xml.bind.annotation.XmlRootElement;
+ 
+@XmlRootElement(name = "GetStudentRequest")
+public class GetStudentRequest {
+	private int id;
+ 
+	public int getId() {
+		return id;
+	}
+ 
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ soap/src/main/java/com/soap/service/StudentService.java
@@ -0,0 +1,48 @@
+package com.soap.service;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.soap.model.Student;
+
+@Service
+
+public class StudentService {
+
+	private List<Student> students = new ArrayList();
+
+	public StudentService() {
+
+		students.add(new Student(1, "Tanuj",22,"A"));
+
+		students.add(new Student(2, "Tanuja",22,"A"));
+
+		students.add(new Student(3, "Tarun",22,"A"));
+
+	}
+
+	public Student getStudentById(int id) {
+
+		return students.stream().filter(s-> s.getId()==id).findFirst().orElse(null);
+
+	}
+
+	public String addStudent(int id, String name, int age,String grade) {
+
+		students.add(new Student(id, name,age,grade));
+
+		return "Student Added Successfully";
+
+	}
+
+	public List<Student> getAllStuents(){
+
+		return students;
+
+	}
+
+}
+ 
